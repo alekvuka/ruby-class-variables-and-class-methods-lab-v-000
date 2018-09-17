25f31aeceb2449f2bf4a09a5f4623a592e75040c
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-17 20:38:20 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+
+  @@number_of_songs = 0
+
+
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist
+    @genre = genre
+
+    @@number_of_songs += 1
+    
+  end 
+
+
+  def self.artists
+
+  end 
+
+
+  def self.count 
+
+  end 
+
+  def self.genres
+
+  end 
+
+   
